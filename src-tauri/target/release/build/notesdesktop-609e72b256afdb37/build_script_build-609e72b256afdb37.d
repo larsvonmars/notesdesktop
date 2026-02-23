f259a62610142fa57de77aa97e3372be00d24b20
Lars Schell <contact@larsschell.ch>
2026-02-23 12:47:14 +0100

    build files

--- src-tauri/target/release/build/notesdesktop-609e72b256afdb37/build_script_build-609e72b256afdb37.d
+++
@@ -1,5 +0,0 @@
-/Users/larsschell/Documents/vscode/notesdesktop/src-tauri/target/release/build/notesdesktop-609e72b256afdb37/build_script_build-609e72b256afdb37.d: build.rs
-
-/Users/larsschell/Documents/vscode/notesdesktop/src-tauri/target/release/build/notesdesktop-609e72b256afdb37/build_script_build-609e72b256afdb37: build.rs
-
-build.rs:
--- src-tauri/target/release/deps/notesdesktop-967ddd9d64163f14.d
+++
@@ -1,5 +0,0 @@
-/Users/larsschell/Documents/vscode/notesdesktop/src-tauri/target/release/deps/notesdesktop-967ddd9d64163f14.d: src/main.rs
-
-/Users/larsschell/Documents/vscode/notesdesktop/src-tauri/target/release/deps/notesdesktop-967ddd9d64163f14: src/main.rs
-
-src/main.rs:
